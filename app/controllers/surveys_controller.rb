a3acc0de04054f8f935bb3d99f5fda31d245b449
sharkby7e <sgquin@gmail.com>
2024-11-15 13:50:54 -0500

    get rid of n+1

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,12 +1,19 @@
 class SurveysController < ApplicationController
   def index
-    @surveys = Survey.all.includes(:survey_responses)
+    @surveys = Survey.all
+
+    survey_responses = SurveyResponse.group(:survey_id, :answer)
+                                     .count
 
     @survey_responses = @surveys.each_with_object({}) do |survey, acc|
-      total = survey.survey_responses.count
+      total = survey_responses.select { |(survey_id, _), _| survey_id == survey.id }.values.sum
+
+      yes_count = survey_responses[[survey.id, 'yes']] || 0
+      no_count = survey_responses[[survey.id, 'no']] || 0
+
       acc[survey.id] = {
-        yes: total > 0 ? ((survey.survey_responses.where(answer: 'yes').count / total.to_f) * 100).round : 0,
-        no: total > 0 ? ((survey.survey_responses.where(answer: 'no').count / total.to_f) * 100).round : 0
+        yes: total > 0 ? ((yes_count / total.to_f) * 100).round : 0,
+        no: total > 0 ? ((no_count / total.to_f) * 100).round : 0
       }
     end
   end
